d1ea6cf74bc82b61eb7cee6c3e0bc910755a5489
kaushalbro <official.kaushalg@gmail.com>
2024-02-26 08:07:24 +0545

    lavavel auth breeze install

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource("products",ProductController::class);
-Route::resource("categories",CategoryController::class);
-Route::resource("orders",OrderController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
