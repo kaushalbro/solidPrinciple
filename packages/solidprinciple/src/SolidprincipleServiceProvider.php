4c3e1ee15e7a37202c0fb2cc8f45cef50832c54a
kaushalbro <official.kaushalg@gmail.com>
2024-02-28 18:01:59 +0545

    remove generated file

--- packages/solidprinciple/src/SolidprincipleServiceProvider.php
+++ packages/solidprinciple/src/SolidprincipleServiceProvider.php
@@ -16,7 +16,7 @@ class SolidprincipleServiceProvider extends ServiceProvider
     public function boot(): void
     {
            $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-//        if ($this->app->runningInConsole()) {
+//        if ($this->app->running6InConsole()) {
             $this->commands([
                 solid::class,
             ]);
