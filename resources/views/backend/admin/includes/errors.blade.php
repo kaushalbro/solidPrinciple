96c60d7a6f86d6d9fd45c15136f4ceff73d88e12
kaushalbro <official.kaushalg@gmail.com>
2024-03-17 15:25:08 +0545

    working in frontend

--- resources/views/backend/admin/includes/errors.blade.php
+++ resources/views/backend/admin/includes/errors.blade.php
@@ -12,20 +12,20 @@
             </div>
         </div>
     @endif
-        @if (session('failed'))
-            <div class="card bg-danger ">
-                <div class="card-header "  style="padding-top: 5px;padding-bottom: 5px;">
-                    <h3 class="card-title">Message:</h3>
-                    <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
-                        </button>
-                    </div>
-                </div>
-                <div class="card-body"  style="padding-top: 5px;padding-bottom: 5px;">
-                    {{ session('failed') }}
+    @if (session('failed'))
+        <div class="card bg-danger ">
+            <div class="card-header "  style="padding-top: 5px;padding-bottom: 5px;">
+                <h3 class="card-title">Message:</h3>
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i>
+                    </button>
                 </div>
             </div>
-        @endif
+            <div class="card-body"  style="padding-top: 5px;padding-bottom: 5px;">
+                {{ session('failed') }}
+            </div>
+        </div>
+    @endif
     @if ($errors->any())
         <div class="card bg-danger ">
             <div class="card-header ">
---
+++ resources/views/backend/admin/product/create.blade.php
@@ -0,0 +1,22 @@
+@extends('backend.master')
+@section('title')
+    Product | create
+@stop
+@section('breadcum')
+    @include('backend.admin.includes.breadcum',['heading'=>"Product","sub_heading"=>'create'])
+@stop
+@section('content')
+    @include("backend.admin.includes.errors")
+    <form class="" action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+         <div class="card-header">
+           <h3 class="card-title">Create</h3>
+         </div>
+         <div class="card-body">
+            @include("backend.admin.product.partial.form",['type'=>'create'])
+          </div>
+    </form>
+@stop
+@push('scripts')
+   @include("backend.admin.product.script")
+@endpush
