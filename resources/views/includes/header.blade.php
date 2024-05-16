ea427bbd9b126680502504ca33cd17b955eb602d
kaushalbro <official.kaushalg@gmail.com>
2024-05-16 17:40:02 +0545

    layout created

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,28 @@
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>  @yield('header_title')  </title>
+  <link rel="icon" type="image/x-icon" href="{{ asset('/admin_resources/image/dummy_logo.jpg') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="robots" content="index, follow" />
+    <meta name="COUNTRY" content="global" />
+    <meta name="LANGUAGE" content="en" />
+    <meta name="REGION" content="global" />
+    <meta property="og:url" content="https://kaushalg.com.np" />
+    <meta property="og:locale" content="en" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <link rel="shortcut icon" href="https://kaushalg.com.np/favicon.ico" />
+    <link rel="canonical" href="https://kaushalg.com.np" />
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+    <style>
+        .header,body{font-family:sans-serif}*,div,section nav{margin:0;padding:0}li,ul{list-style:none}.header,aside,nav .list h1,ul{text-align:center}.upper h3,header div h6{display:inline-block}*{box-sizing:border-box}html{scroll-behavior:smooth}h1,h2,h3,h4,h5,h6{font-size:1.5rem;font-weight:700}p{font-size:.9rem}h1{color:#fff}h1:hover{color:#000}body:hover{background-color:#1aed8e}header div h3{padding-right:5rem}.center{display:flex;align-items:center;justify-content:center}header div h6{float:left;color:red;font-size:.7rem;border:solid;padding-left:0;border-radius:1rem}body{background-color:#08f7ff;padding-left:.5rem}.header:hover{background-color:#f0eca8}.header{background-color:#12fc31;font-size:1.5rem;width:100%;height:100px}div,section nav{background-color:#ff0}aside{background-color:#89dff0;padding:10px;max-height:200px;min-height:200px;border-style:solid;border-top-color:#fff;border-bottom-color:#fff}a{text-decoration:none}.link{padding:6px}nav .list h1{background-color:#89c5f0}.nav1{width:20%;padding-top:.3rem}.link:hover{background-color:#96ff45}.link1{background-color:#f55bed}.link2{background-color:#f5cf5b}.link3{background-color:#4cf58a}.link4{background-color:orange}.link5{background-color:#5a8afa}.article{width:60%;height:20rem}.lower,.upper{height:50%;margin:3px;text-align:center}.nav2{float:left}.header h3{margin-top:.5rem;padding-top:.5rem}.upper h3,div .lfooter,footer div{padding-top:5rem}.upper{border:solid;border-top-color:#fff;border-bottom-color:#fff;background-color:pink}.lowerheader,.upperheader{border:solid #fff;padding:.2rem}.lower{background-color:#f73b45}.upperheader{background-color:#1f6bed}.lowerheader{background-color:#ed9b1f;border-radius:1rem}footer{height:100%}footer div{background-color:#6fed4c}footer div h3{border:solid #ff0;text-align:center;background-color:#fc03db;height:350px;padding-top:10rem}
+    </style>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+</head>
+
---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@include('includes.header')
+@section('header_title')
+    @yield('title')
+@stop
+<body>
+    @include('includes.navbar')
+    @include('includes.sidebar')
+    @yield('breadcrumb')
+        <section class="">
+                @yield('content')
+        </section>
+    @include('includes.footer')
+    @include('includes.scripts')
+    @stack('scripts')
+</body>
+</html>
+
+
