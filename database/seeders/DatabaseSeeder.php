dbd35a7dee05e7493d866b77e98f7d1d8e9b702f
kaushalbro <official.kaushalg@gmail.com>
2024-08-17 12:51:19 +0545

    Minor change Bux fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
@@ -18,21 +19,33 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $users = [];
-
-        for ($i = 100010; $i < 2000000; $i++) {
-            $users= [
-                'name' => 'name_' . $i,
-                'email' => 'official.cyberbar11+' . $i . '@gmail.com',
-                'email_verified_at' => null,
-                'password' =>'$2y$10$u53dA8KZ/dF72AqKh8461OLOQMx5XdPyJOvHJWoyszDrtFmU7PPja',
-                'remember_token' => Str::random(32),
-                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
-                'status' => 1,
-                'super_admin' => 0,
-            ];
-            DB::table('users')->insert($users);
-        }
+        $user=DB::table('users');
+        $users= [
+            'name' => 'Kaushal Ghimire',
+            'email' => 'official.kaushalg@gmail.com',
+            'email_verified_at' =>  Carbon::now()->format('Y-m-d H:i:s'),
+            'password' =>Hash::make('password'),
+            'remember_token' => Str::random(32),
+            'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            'status' => 1,
+            'super_admin' => 1,
+        ];
+        $user->updateOrInsert($users);
+//        for ($i = 100010; $i < 2000000; $i++) {
+//            $users= [
+//                'name' => 'name_' . $i,
+//                'email' => 'official.cyberbar11+' . $i . '@gmail.com',
+//                'email_verified_at' => null,
+//                'password' =>'$2y$10$u53dA8KZ/dF72AqKh8461OLOQMx5XdPyJOvHJWoyszDrtFmU7PPja',
+//                'remember_token' => Str::random(32),
+//                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+//                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+//                'status' => 1,
+//                'super_admin' => 0,
+//            ];
+//            $user->insert($users);
+//        }
         try {
 //            DB::table('users')->updateOrInsert($users);
         }catch (\Exception $exception){
